dc700f474dd866e64853121ef3b00fb3143b8de4
Sajan Gurung <gsajan21@gmail.com>
2023-10-26 19:31:02 -0700

    changed the template engine to ejs

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "static")));
 const rootRoutes = require("./routes/root");
 app.use("/", rootRoutes);
