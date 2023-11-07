f295ed5153ba420d8b11f7b698df82095cae7f05
kotaro <kotakota0313@gmail.com>
2023-11-06 20:57:02 -0800

    set up migration

---
+++ migrations/1699328722412_test-migration.js
@@ -0,0 +1,30 @@
+/* eslint-disable camelcase */
+
+//exports.shorthands = undefined;
+
+/**
+ * @param {import("node-pg-migrate/dist/types").MigrationBuilder}
+ */
+
+exports.up = (pgm) => {
+  pgm.createTable("test_table", {
+    id: "id",
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    test_string: {
+      type: "varchar(1000)",
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param {import("node-pg-migrate/dist/types").MigrationBuilder}
+ */
+
+exports.down = (pgm) => {
+  pgm.dropTable("test_table");
+};
